8e26edbc97f456fdf85c5ac59a257671a3a67d8e
Jessica Hathaway <hathaway.jess@gmail.com>
2016-02-07 15:54:17 -0700

    Finished some problems

--- arraysPractice.js
+++ arraysPractice.js
@@ -6,6 +6,10 @@ var arr = [10,20,30];
 
   //Code Here
 
+  function first(arr) {
+    return arr[0];
+  }
+
 
 //Next problem
 
@@ -17,33 +21,58 @@ var arr = [40,50,60];
 
   //Code Here
 
+ function last(arr) {
+  return arr[arr.length - 1];
+ }
+
 
 //Next Problem
 
 
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
-//Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
+//Create a function named 'looper' that is given family as it's only argument, loops through the given array, and 
+//alerts every item in the array.
 
   //Code Here
-
+  function looper(family) {
+    for (var i = 0; i < family.length; i++) {
+      alert(family[i]);
+    }
+  }
 
 //Next problem
 
 
 
 var letters = ['A', 'B', 'C', 'D', 'E'];
-//Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
+//Write a function called reversedLooper that is given letters as it's only argument and 
+//loops through the given array backwards alerting every item in the array starting at the end.
 
   //Code Here
 
+function reversedLooper(letters) {
+  for (var i = letters.length; i >= 0; --i) {
+    alert(letters[i]);
+  }
+}
 
 //Next Problem
 
 
 var nums = [1,2,3,6,22,98,45,23,22,12];
-//Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
+//Write a function named evenFinder that is given nums as it's only argument 
+//and removes all values that aren't even from the given array.
 
   //Code Here
+
+  function evenFinder(nums) {
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] % 2 !== 0) {
+        nums.splice(i, 1);
+      }
+    }
+    return nums;
+  }
 
 
 //Next problem
